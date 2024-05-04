98a31c75ff70049944fc277124e6102bebd8f493
Alvin Tang <alvin.tang@anu.edu.au>
2024-05-04 17:35:03 +1000

    Refine mqtt_connect() and listen_request()

--- publisher.c
+++ publisher.c
@@ -4,16 +4,19 @@
 #include <stdbool.h>
 #include "MQTTClient.h"
 
-// 3 minutes timeout
-#define TIMEOUT 600000
+// Timeout for receiving incoming messages: around 17 minutes
+#define TIMEOUT 1048576
 
-// Helper functions
-int publish(char *url, char *client_id);
-int message_arrive(void *context, char *topicName, int topicLen, MQTTClient_message *message);
-void execute_publish();
+// Helper functions for receiving and publishing messages
+static MQTTClient *mqtt_connect(char *url, int instance);
+static void listen_request(MQTTClient *client);
+static void publish_counter(MQTTClient *client);
+static void mqtt_disconnect(MQTTClient *client);
 
-// Global variables
-char *topics[] = {"request/qos", "request/delay", "request/instancecount"};
+// Global variables: topics for subscription
+char *subs_topics[] = {"request/qos", "request/delay", "request/instancecount"};
+
+// Global variables: values from the subscribed topics
 int values[] = {-1, -1, -1};
 const int *qos = &values[0];
 const int *delay = &values[1];
@@ -21,61 +24,96 @@ const int *instance_count = &values[2];
 
 /**
  * The main function to execute the publisher program.
+ * To execute the program, the command should include the MQTT broker's hostname
+ * and its port, as well as the instance number of this publisher.
  */
 int main(int argc, char *argv[]) {
     // Parse the command input
     if (argc != 4) {
-        fprintf(stderr, "Usage: %s <Broker hostname> <Port> <Client ID>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <Broker hostname> <Port> <Instance>\n", argv[0]);
         exit(EXIT_SUCCESS);
     }
-    char *url = malloc(strlen(argv[0]) + strlen(argv[1]) + 9);
+
+    // Create the full URL to the broker using its hostname and port number
+    char *url = malloc(strlen(argv[1]) + strlen(argv[2]) + 9);
     sprintf(url, "mqtt://%s:%s", argv[1], argv[2]);
-    char *client_id = argv[3];
 
-    // Connect to the broker to perform the required tasks
-    int rc = publish(url, client_id);
+    // Check that the instance number is within the range from 1 to 5
+    int instance = atoi(argv[3]);
+    if (instance < 1 || instance > 5) {
+        fprintf(stderr, "Error: instance number must be in between 1 and 5\n");
+        exit(EXIT_FAILURE);
+    }
 
-    // Termiante the program and return rc
+    // Initialise an MQTTClient handle and connect to the broker
+    MQTTClient *client = mqtt_connect(url, instance);
     free(url);
-    return rc;
+
+    // Listen for incoming messages (QoS, delay and instance count)
+    listen_request(client);
+
+    // Publish messages to the broker
+    publish_counter(client);
+
+    // Disconnect from the broker
+    mqtt_disconnect(client);
+
+    return 0;
 }
 
 /**
- * Connect to the MQTT broker and subscribe to the set of "request" topics.
- * Once new values in these topics are observed, start publishing.
+ * Connect to the MQTT broker and set up the MQTTClient handle.
  * 
  * @param url URL to the MQTT broker
- * @param id Client ID of this publisher
+ * @param instance Instance number of this publisher
+ * @return the MQTTClient handle object
  */
-int publish(char *url, char *id) {
+static MQTTClient *mqtt_connect(char *url, int instance) {
+    // Create the MQTTClient handle
+    MQTTClient *client = (MQTTClient *)malloc(sizeof(MQTTClient));
+
     // Initialise the MQTT client configurations
-    MQTTClient client;
     MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
-    // MQTTClient_message pubmsg = MQTTClient_message_initializer;
-    // MQTTClient_deliveryToken token;
-    int rc;
-    char *client_id = malloc(strlen(id) + 5);
-    sprintf(client_id, "pub-%s", id);
+    char *client_id = malloc(6);
+    sprintf(client_id, "pub-%d", instance);
 
     // Create the MQTT client
-    MQTTClient_create(&client, url, client_id, MQTTCLIENT_PERSISTENCE_NONE, NULL);
+    MQTTClient_create(client, url, client_id, MQTTCLIENT_PERSISTENCE_NONE, NULL);
     conn_opts.keepAliveInterval = 1024;
     conn_opts.cleansession = true;
 
     // Establish a connection with the MQTT broker
-    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS) {
-        printf("Failed to connect, return code %d\n", rc);
+    int status = MQTTClient_connect(*client, &conn_opts);
+    if (status != MQTTCLIENT_SUCCESS) {
+        fprintf(stderr, "Failed to connect, return code %d\n", status);
+        mqtt_disconnect(client);
         exit(EXIT_FAILURE);
     }
 
+    return client;
+}
+
+/**
+ * Subscribe to the three specified topics and listen to incoming messages
+ * until all values have been received.
+ * 
+ * If all three values are retained on the broker's record, they should be
+ * immediately received and written to the global variables. Otherwise, this
+ * functions waits for any incoming messages on these topics until all these
+ * values are received or timeout expires.
+ * 
+ * @param client pointer to the MQTTClient handle object
+ */
+static void listen_request(MQTTClient *client) {
     // Subscribe to the three specified topics
-    char *topics[] = {"request/qos", "request/delay", "request/instancecount"};
-    int qos_send[] = {0, 0, 0};
-    int status = MQTTClient_subscribeMany(client, 3, topics, qos_send);
+    int subs_qos[] = {0, 0, 0};
+    int status = MQTTClient_subscribeMany(*client, 3, subs_topics, subs_qos);
     if (status != MQTTCLIENT_SUCCESS) {
-        fprintf(stdout, "Error subscribing to the three topics\n");
+        fprintf(stderr, "Error subscribing to the three topics\n");
+        return;
     }
 
+    // Variables for managing incoming messages
     MQTTClient_message *message = NULL;
     char *topic = NULL;
     int topic_length = -1;
@@ -83,35 +121,45 @@ int publish(char *url, char *id) {
     // Read incoming messages at subscribed topics
     while (*qos == -1 || *delay == -1 || *instance_count == -1) {
         // Wait for an income message
-        MQTTClient_receive(client, &topic, &topic_length, &message, TIMEOUT);
+        MQTTClient_receive(*client, &topic, &topic_length, &message, TIMEOUT);
 
         // The message pointer is NULL when timeout expires
         if (message == NULL) {
             fprintf(stderr, "Timeout\n");
         }
 
-        // Determine the topic
+        // Determine the topic and write the value to the relevant variable
         for (int i = 0; i <= 2; i++) {
-            if (strcmp(topic, topics[i]) == 0) {
+            if (strcmp(topic, subs_topics[i]) == 0) {
                 values[i] = atoi((char *)message->payload);
             }
         }
 
+        // Free the memory allocated for the incoming message
         MQTTClient_freeMessage(&message);
         MQTTClient_free(topic);
     }
-
-    execute_publish();
-
-    MQTTClient_disconnect(client, 10000);
-    MQTTClient_destroy(&client);
-
-    return rc;
 }
 
-void execute_publish() {
-    fprintf(stdout, "Execute!\n");
+/**
+ * Publish the incrementing counter messages to the MQTT broker.
+ * 
+ * @param client pointer to the MQTTClient handle object
+ */
+static void publish_counter(MQTTClient *client) {
+    (void)client;
     fprintf(stdout, "QoS: %d\n", *qos);
     fprintf(stdout, "Delay: %d\n", *delay);
     fprintf(stdout, "Instance count: %d\n", *instance_count);
+}
+
+/**
+ * Disconnect from the MQTT broker before terminating the program.
+ * 
+ * @param client pointer to the MQTTClient handle object
+ */
+static void mqtt_disconnect(MQTTClient *client) {
+    MQTTClient_disconnect(*client, 10000);
+    MQTTClient_destroy(client);
+    free(client);
 }
