7fac74d3253f6994648f8313fe01f7df258e160c
Alvin Tang <alvin.tang@anu.edu.au>
2024-05-06 11:22:26 +1000

    Collect statistics with the analyser

--- analyser.c
+++ analyser.c
@@ -3,16 +3,28 @@
 #include <string.h>
 #include <stdbool.h>
 #include <time.h>
+#include <sys/time.h>
 #include "MQTTClient.h"
 
 // Global variables: string buffer size
 #define BUFFER_SIZE 128
+
+// Analyse messages for 60 seconds
+#define TIME_LIMIT 60
 
 // Helper functions
 static MQTTClient *mqtt_connect(char *url);
 static void analyse(MQTTClient *client, short int broker_to_analyser_qos,
                     short int qos, short int delay, short int instance_count);
 static void mqtt_disconnect(MQTTClient *client);
+static void listen_counter(MQTTClient *client);
+
+long long current_timestamp() {
+    struct timeval te;
+    gettimeofday(&te, NULL);
+    long long milliseconds = te.tv_sec * 1000LL + te.tv_usec / 1000;
+    return milliseconds;
+}
 
 /**
  * The mian function executes the analyser.
@@ -34,15 +46,22 @@ int main(int argc, char *argv[]) {
     MQTTClient *client = mqtt_connect(url);
 
     // Perform the analysis
-    int qos_levels[] = {0, 1, 2};
-    int delay_length[] = {0, 1, 2, 4};
-    int instance_counts[] = {1, 2, 3, 4, 5};
-    for (int h = 0; h < 3; h++)
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 4; j++)
-                for (int k = 0; k < 5; k++)
-                    analyse(client, qos_levels[h], qos_levels[i],
-                            delay_length[j], instance_counts[k]);
+    fprintf(stdout, "Analysing with:\n"
+        "\tQoS from broker to analyser: %d\n"
+        "\tQoS from publisher to broker: %d\n"
+        "\tDelay time: %d\n"
+        "\tNumber of active publishers: %d\n", 2, 2, 0, 5);
+    analyse(client, 0, 0, 4, 5);
+
+    // int qos_levels[] = {0, 1, 2};
+    // int delay_length[] = {0, 1, 2, 4};
+    // int instance_counts[] = {1, 2, 3, 4, 5};
+    // for (int h = 0; h < 3; h++)
+    //     for (int i = 0; i < 3; i++)
+    //         for (int j = 0; j < 4; j++)
+    //             for (int k = 0; k < 5; k++)
+    //                 analyse(client, qos_levels[h], qos_levels[i],
+    //                         delay_length[j], instance_counts[k]);
 
     // Disconnect from the broker before termination
     mqtt_disconnect(client);
@@ -138,8 +157,70 @@ static void analyse(MQTTClient *client, short int broker_to_analyser_qos,
     MQTTClient_publishMessage(*client, "request/delay", &delay_message, NULL);
     MQTTClient_publishMessage(*client, "request/instancecount", &instance_count_message, NULL);
 
+    // Listen to the current topic for the counter's responses
+    listen_counter(client);
+
     // Unsubscribe from the current topic
     MQTTClient_unsubscribe(*client, topic);
+}
+
+static void listen_counter(MQTTClient *client) {
+    time_t end_time = time(NULL) + TIME_LIMIT;
+
+    // The last counter number received
+    int last_count = -1;
+    // The total number of messages
+    int number_of_messages = 0;
+    // The number of out-of-order messages
+    int out_of_order_count = 0;
+    // Total time used for delays
+    double delays = 0;
+
+    while (time(NULL) < end_time) {
+        MQTTClient_message *message = NULL;
+
+        char *topic_name = NULL;
+        int topic_len = -1;
+
+        long long start_time = current_timestamp();
+        if (MQTTClient_receive(*client, &topic_name, &topic_len, &message, 5) != MQTTCLIENT_SUCCESS) {
+            continue;
+        }
+
+        if (topic_name != NULL) {
+            int this_count = atoi((char *)message->payload);
+            if (this_count != last_count + 1) {
+                out_of_order_count++;
+                number_of_messages++;
+            }
+            else {
+                long long end_time = current_timestamp();
+                delays += end_time - start_time;
+                last_count = this_count;
+                number_of_messages++;
+            }
+
+            MQTTClient_freeMessage(&message);
+            MQTTClient_free(topic_name);
+        }
+    }
+
+    // The average rate of messages
+    double average_msg_rate = number_of_messages / TIME_LIMIT;
+    // The average rate of out-of-order messages
+    double out_of_order_msg_rate = out_of_order_count / number_of_messages;
+    // The average delay
+    double avg_delay = delays * 1000 / number_of_messages;
+
+    // Print the statistics to terminal output
+    fprintf(stdout,
+            "last_count: %d\n"
+            "num of msg: %d\n"
+            "average msg rate: %f per second\n"
+            "out of order rate: %.2f%%\n"
+            "average delay: %f\n",
+        last_count, number_of_messages,
+        average_msg_rate, out_of_order_msg_rate / 100, avg_delay);
 }
 
 /**
